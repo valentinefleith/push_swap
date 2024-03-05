4430e4dedd43922f47f651abf8791070fa32ec2c
valentinefleith <valentine.fleith@gmail.com>
2024-03-05 13:56:30 +0100

    add parse functions

--- include/pushswap.h
+++ include/pushswap.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   pushswap.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vafleith <vafleith@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 15:49:17 by vafleith          #+#    #+#             */
-/*   Updated: 2024/02/28 15:51:48 by vafleith         ###   ########.fr       */
+/*   Updated: 2024/03/05 13:56:22 by vafleith         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,17 @@
 #include "libft.h"
 #include <stdlib.h>
 
+# define MALLOC_ERROR 1
+# define ARGUMENT_ERROR 2
+
+typedef struct s_stack
+{
+	void			*content;
+	struct s_stack	*next;
+	struct s_stack 	*previous;
+}					t_stack;
+
+void parse_args(int argc, char **argv);
 // prototypes here
 
 #endif
--- src/parse_args.c
+++ src/parse_args.c
@@ -6,8 +6,50 @@
 /*   By: vafleith <vafleith@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 16:05:41 by vafleith          #+#    #+#             */
-/*   Updated: 2024/02/28 16:05:55 by vafleith         ###   ########.fr       */
+/*   Updated: 2024/03/05 13:56:19 by vafleith         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pushswap.h"
+#include "../include/pushswap.h"
+
+static t_stack *parse_one_arg(char *argv)
+{
+	char **args;
+	t_stack *stack;
+
+	if (!is_valid_one(argv))
+	{
+		ft_printf("Fromat error.\n");
+		exit(ARGUMENT_ERROR);
+	}
+	args = ft_split(argv);
+	if (args == NULL)
+		exit(MALLOC_ERROR);
+	stack = load_data(args);
+	free(args);
+	if (stack == NULL)
+		exit(MALLOC_ERROR);
+	return to_linked_list(args);
+}
+
+static t_stack *parse_several_args(char **argv)
+{
+	if (!is_valid_several(argv))
+	{
+		ft_printf("Format error.\n");
+		exit(ARGUMENT_ERROR);
+	}
+	return (argv);
+}
+
+char **parse_args(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		ft_printf("Error. You need to enter numbers in argument\n");
+		exit(ARGUMENT_ERROR);
+	}
+	if (argc == 2)
+		return parse_one_arg(argv[1]);
+	return parse_several_args(argv);
+}
