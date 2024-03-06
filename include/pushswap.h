77c3b05f7a36f083d2573994888478b636cbe019
valentinefleith <valentine.fleith@gmail.com>
2024-03-06 16:27:31 +0100

    add utils for doubly linked lists

--- include/pushswap.h
+++ include/pushswap.h
@@ -6,7 +6,7 @@
 /*   By: vafleith <vafleith@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 15:49:17 by vafleith          #+#    #+#             */
-/*   Updated: 2024/03/06 15:55:12 by vafleith         ###   ########.fr       */
+/*   Updated: 2024/03/06 16:27:14 by vafleith         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,20 @@
 
 typedef struct s_stack
 {
-	void			*content;
+	int				content;
 	struct s_stack	*next;
 	struct s_stack 	*previous;
 }					t_stack;
 
 t_stack *parse_args(int argc, char **argv);
+t_stack *arr_to_linked_list(char **strs);
+
+// DOUBLY LINKED LISTS UTILS
+t_stack *ft_stacknew(int content);
+t_stack *ft_stacklast(t_stack *stack);
+void ft_stackadd_back(t_stack **stack, t_stack **new);
+
+
 // prototypes here
 
 #endif
---
+++ src/linked_lists_utils.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   linked_lists_utils.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vafleith <vafleith@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/06 16:13:32 by vafleith          #+#    #+#             */
+/*   Updated: 2024/03/06 16:26:35 by vafleith         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pushswap.h"
+
+t_stack *ft_stacknew(int content)
+{
+	t_stack *node;
+
+	node = malloc(sizeof(t_stack));
+	if (node == NULL)
+		return (NULL);
+	node->content = content;
+	node->next = NULL;
+	node->prev = NULL;
+	return (node);
+}
+
+t_stack *ft_stacklast(t_stack *stack)
+{
+	if (!stack)
+		return NULL;
+	while (stack->next)
+		stack = stack->next;
+	return stack;
+}
+
+void ft_stackadd_back(t_stack **stack, t_stack **new)
+{
+	t_stack *last;
+
+	if (!stack)
+		return;
+	if (!*stack)
+	{
+		*stack = *new;
+		return;
+	}
+	last = ft_stacklast(*stack);
+	last->next = *new;
+	new->prev = *last;
+}
